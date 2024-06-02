eab74414ca78a9300e9bfc973bd39b6b50518583
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-03 03:44:41 +0530

    also completed the mobile responsiveness with a slider navbar, used headlessui of react.

--- shopiverse/src/components/navbar/Navbar.jsx
+++ shopiverse/src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useContext } from 'react';
 import myContext from '../../context/data/myContext';
 import { Link } from 'react-router-dom';
 import { FiSun } from "react-icons/fi";
 import { BsFillCloudSunFill } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
+import {Dialog, Transition} from "@headlessui/react";
 
 function Navbar() {
 
@@ -12,7 +14,7 @@ function Navbar() {
   const [open , setOpen] = React.useState(false);
 
   return (
-    <>
+    <div className="bg-white sticky top-0 z-50  "  >
     {/* desktop  */}
     <header className="relative bg-white">
         <p className="flex h-10 items-center justify-center bg-pink-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8" style={{ backgroundColor: mode === 'dark' ? 'rgb(62 64 66)' : '', color: mode === 'dark' ? 'white' : '', }}>
@@ -108,7 +110,92 @@ function Navbar() {
           </div>
         </nav>
       </header>
-    </>
+    
+    {/* Mobile menu */}
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-40 flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(40, 44, 52)' : '', color: mode === 'dark' ? 'white' : '', }}>
+                <div className="flex px-4 pb-2 pt-28">
+                  <button
+                    type="button"
+                    className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                    onClick={() => setOpen(false)}
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <RxCross2 />
+                  </button>
+                </div>
+                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                  
+                  <Link to={'/allproducts'} className="text-sm font-medium text-gray-900 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                    All Products
+                  </Link>
+                  <div className="flow-root">
+                    <Link to={'/order'} style={{ color: mode === 'dark' ? 'white' : '', }} className="-m-2 block p-2 font-medium text-gray-900">
+                      Order
+                    </Link>
+                  </div>
+
+                  <div className="flow-root">
+                    <Link to={'/dashboard'} className="-m-2 block p-2 font-medium text-gray-900" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      admin
+                    </Link>
+                  </div>
+
+                  <div className="flow-root">
+                    <a className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Logout
+                    </a>
+                  </div>
+                  <div className="flow-root">
+                    <Link to={'/'} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer">
+                      <img
+                        className="inline-block w-10 h-10 rounded-full"
+                        src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
+                        alt="Dan_Abromov" />                                        </Link>
+                  </div>
+                </div>
+
+                <div className="border-t border-gray-200 px-4 py-6">
+                  <a href="#" className="-m-2 flex items-center p-2">
+                    <img
+                      src="img/indiaflag.png"
+                      alt=""
+                      className="block h-auto w-5 flex-shrink-0"
+                    />
+                    <span className="ml-3 block text-base font-medium text-gray-900" style={{ color: mode === 'dark' ? 'white' : '', }}>INDIA</span>
+                    <span className="sr-only">, change currency</span>
+                  </a>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+    </div>
   )
 }
 
