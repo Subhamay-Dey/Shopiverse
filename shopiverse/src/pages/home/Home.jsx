5038fe816d01ac091ac440e82859db13831c56c7
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-05 04:46:16 +0530

    filter component sucessfully finished which I have also given the dark mode styling extracting the mode from the myContext by useContext and rendered it in the home page.

--- shopiverse/src/pages/home/Home.jsx
+++ shopiverse/src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 import HeroSection from '../../components/heroSection/HeroSection';
+import Filter from '../../components/filter/Filter';
 
-export default function Home() {
+
+ function Home() {
   const context = useContext(myContext);
   return (
     <Layout>
       <HeroSection/>
+      <Filter/>
     </Layout>
   )
 }
+export default Home;
