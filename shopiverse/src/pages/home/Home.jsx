269319c14f8ea4c91da747c84836c520ac5ea889
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-05 06:14:52 +0530

    product card component finally completed and also rendered in the home.jsx

--- shopiverse/src/pages/home/Home.jsx
+++ shopiverse/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 import HeroSection from '../../components/heroSection/HeroSection';
 import Filter from '../../components/filter/Filter';
+import ProductCard from '../../components/productCard/ProductCard';
 
 
  function Home() {
@@ -11,6 +12,7 @@ import Filter from '../../components/filter/Filter';
     <Layout>
       <HeroSection/>
       <Filter/>
+      <ProductCard/>
     </Layout>
   )
 }
