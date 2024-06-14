62945b11f6d01365b98a3eb860b6990b5bbba23c
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-14 20:37:53 +0530

    completed the dashboard component of the admin also wrapped it with the layout component and you can navigate to the admin page by clicking in the admin option in the navbar.

--- shopiverse/src/components/footer/Footer.jsx
+++ shopiverse/src/components/footer/Footer.jsx
@@ -69,7 +69,7 @@ function Footer() {
                         </div>
                     </Link>
                     <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4" style={{ color: mode === 'dark' ? 'white' : '' }}>© 2023 Shopiverse —
-                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank" style={{ color: mode === 'dark' ? 'white' : '' }}>www.shopiverse.com</a>
+                        <a href="" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank" style={{ color: mode === 'dark' ? 'white' : '' }}>www.shopiverse.com</a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="text-gray-500">
