d04dc050eb5be782d9273e8db1f9f5cbc6dc8ef8
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-14 20:10:33 +0530

    Successfully completed the modal component which will be rendered when the buy now button will be clicked on the cart page :)

--- shopiverse/src/pages/cart/Cart.jsx
+++ shopiverse/src/pages/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import myContext from '../../context/data/myContext';
 import Layout from '../../components/layout/Layout';
+import Modal from '../../components/modal/modal';
 
 
 function Cart() {
@@ -54,7 +55,7 @@ function Cart() {
               type="button"
               className="w-full  bg-violet-600 py-2 text-center rounded-lg text-white font-bold "
             >
-              Buy Now
+              {<Modal/>}
             </button>
           </div>
         </div>
