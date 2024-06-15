b6f590f1bcb5605628f6b8eb18be40957e31dcf8
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-16 01:00:47 +0530

    Added cartSlice feature to store reducer in store.jsx :-) react-redux-toolkit is just awesome ;))

--- shopiverse/src/toolkit/store.jsx
+++ shopiverse/src/toolkit/store.jsx
@@ -1,8 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit"
+import cartSlice from "./cartSlice"
 
 export const store = configureStore({
   reducer: {
-
+      cart: cartSlice,
   },
   devTools: true,
 })
