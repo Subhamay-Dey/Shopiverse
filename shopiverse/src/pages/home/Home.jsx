d729cfb41c6a2b955fbb44456ecc9b495f7f013f
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-17 17:00:41 +0530

    finally completed the redux-toolkit part but now the data is fixed later the redux will operate with the actual data of the database.

--- shopiverse/src/pages/home/Home.jsx
+++ shopiverse/src/pages/home/Home.jsx
@@ -6,12 +6,32 @@ import Filter from '../../components/filter/Filter';
 import ProductCard from '../../components/productCard/ProductCard';
 import Track from '../../components/track/Track';
 import Testimonial from '../../components/testimonial/Testimonial';
-
+import {useDispatch, useSelector} from "react-redux"
+import { addProduct, deleteProduct } from '../../toolkit/cartSlice';
 
  function Home() {
+
   const context = useContext(myContext);
+
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart);
+
+  console.log(cartItem);
+
+  const addCart = () => {
+    dispatch(addProduct("shirt"));
+  }
+
+  const deleteCart = () => {
+    dispatch(deleteProduct("shirt"));
+  }
+
   return (
     <Layout>
+      <div className="flex justify-center gap-4">
+        <button className='bg-gray-600 p-2' onClick={() => addCart()}>add</button>
+        <button className='bg-gray-600 p-2' onClick={() => deleteCart()}>delete</button>
+      </div>
       <HeroSection/>
       <Filter/>
       <ProductCard/>
