c668dbd064fe58a59bfbf919dfa17b1db6cddf58
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-28 23:55:06 +0530

    sign up page successfully completed.

--- shopiverse/src/pages/registration/SignUp.jsx
+++ shopiverse/src/pages/registration/SignUp.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className=' flex justify-center items-center h-screen'>
+    <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
+        <div className="">
+            <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
+        </div>
+        <div>
+            <input type="email"
+                name='email'
+                className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                placeholder='Email'
+            />
+        </div>
+        <div>
+            <input
+                type="password"
+                className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                placeholder='Password'
+            />
+        </div>
+        <div className=' flex justify-center mb-3'>
+            <button
+                className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
+                Signup
+            </button>
+        </div>
+        <div>
+            <h2 className='text-white'>Have an account <Link className=' text-red-500 font-bold' to={'/login'}>Login</Link></h2>
+        </div>
+    </div>
+</div>
   )
 }
 
