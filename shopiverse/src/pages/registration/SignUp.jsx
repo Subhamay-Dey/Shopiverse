7105e4699dfe34abc9934f74113364b3d29f7539
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-03 23:57:39 +0530

    updated the signup component.

--- shopiverse/src/pages/registration/SignUp.jsx
+++ shopiverse/src/pages/registration/SignUp.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function SignUp() {
+
   return (
     <div className=' flex justify-center items-center h-screen'>
     <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
         <div className="">
             <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
+        </div>
+        <div>
+            <input type="text"
+                name='name'
+                className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                placeholder='Name'
+            />
         </div>
         <div>
             <input type="email"
